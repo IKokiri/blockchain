53af1a0696d286595e5c47c0ea1c0b4375ae0927
Luiz Mendes <luiz.mendes@in8.com.br>
2021-12-20 21:39:01 -0300

    style: add edit button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Main extends Component {
-
   render() {
     return (
       <div id="content">
+        
         <h1>Adicionar Produto</h1>
-        <form onSubmit={(event) => {
-          event.preventDefault()
-          const name = this.productName.value
-          const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
-          this.props.createProduct(name, price)
-        }}>
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            const name = this.productName.value;
+            const price = window.web3.utils.toWei(
+              this.productPrice.value.toString(),
+              "Ether"
+            );
+            this.props.createProduct(name, price);
+          }}
+        >
           <div className="form-group mr-sm-2">
             <input
               id="productName"
               type="text"
-              ref={(input) => { this.productName = input }}
+              ref={input => {
+                this.productName = input;
+              }}
               className="form-control"
               placeholder="Nome do produto"
-              required />
+              required
+            />
           </div>
           <div className="form-group mr-sm-2">
             <input
               id="productPrice"
               type="text"
-              ref={(input) => { this.productPrice = input }}
+              ref={input => {
+                this.productPrice = input;
+              }}
               className="form-control"
               placeholder="Preço do produto (em Ether)"
-              required />
+              required
+            />
           </div>
-          <button type="submit" className="btn btn-primary">Adicionar produto</button>
+          <button type="submit" className="btn btn-primary">
+            Adicionar produto
+          </button>
         </form>
         <p>&nbsp;</p>
         <h2>Comprar produto</h2>
@@ -50,28 +63,66 @@ class Main extends Component {
                 <tr key={key}>
                   <th scope="row">{product.id.toString()}</th>
                   <td>{product.name}</td>
-                  <td>{window.web3.utils.fromWei(product.price.toString(), "ether")} Eth</td>
+                  <td>
+                    {window.web3.utils.fromWei(
+                      product.price.toString(),
+                      "ether"
+                    )}{" "}
+                    Eth
+                  </td>
                   <td>{product.owner}</td>
                   <td>
-                    {!product.purchased
-                      ? <button
-                        name={product.id}
-                        value={product.price}
-                        onClick={(event) => {
-                          this.props.purchaseProduct(event.target.name, event.target.value)
-                        }}
-                        >
-                          Comprar
-                        </button>
-                      : null
-                    }
+                    <div class="row">
+                      <div class="col">
+                        {!product.purchased ? (
+                          <button
+                            className="btn btn-success"
+                            name={product.id}
+                            value={product.price}
+                            onClick={event => {
+                              this.props.purchaseProduct(
+                                event.target.name,
+                                event.target.value
+                              );
+                            }}
+                          >
+                            Comprar
+                          </button>
+                        ) : null}
+                      </div>
+                      <div class="col">
+                        {!product.purchased ? (
+                          <button
+                            className="btn btn-info"
+                            name={product.id}
+                            value={product.price}
+                            onClick={event => {
+                              this.props.purchaseProduct(
+                                event.target.name,
+                                event.target.value
+                              );
+                            }}
+                          >
+                            Editar
+                          </button>
+                        ) : null}
+                      </div>
+                    </div>
+                    <div></div>
                   </td>
                 </tr>
-              )
+              );
             })}
           </tbody>
         </table>
-        <p><a href="https://ropsten.etherscan.io/address/0x781c71bfe45d1c5d81fca75d17bb589cc72d63fc" target="_blank">Informação do contrato</a></p>
+        <p>
+          <a
+            href="https://ropsten.etherscan.io/address/0x781c71bfe45d1c5d81fca75d17bb589cc72d63fc"
+            target="_blank"
+          >
+            Informação do contrato
+          </a>
+        </p>
       </div>
     );
   }
